f2e028e932c74295281e05a94a971a0fc9bcb064
Oky Bagus <okkybagus87@gmail.com>
2021-04-06 07:49:52 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main(){
+	cout << "Menu Utama" << endl;
+	cout << "1. Main" << endl;
+	cout << "2. Cara bermain" << endl;
+	cout << "3. Keluar" << endl;
 
     return 0;
 }
