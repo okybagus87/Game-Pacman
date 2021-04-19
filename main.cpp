cea9beede8bca4da869359adb25e25c3558069a8
Oky Bagus <okkybagus87@gmail.com>
2021-04-19 09:29:16 +0700

    penambahan if pada pilihan 1 saja

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+#include <graphics.h>
+#include "map.h"
 using namespace std;
 
 int main(){
+	int pilihan;
+	
 	cout << "Menu Utama" << endl;
 	cout << "1. Main" << endl;
 	cout << "2. Skor" << endl;
 	cout << "3. Cara bermain" << endl;
 	cout << "4. Keluar" << endl;
+	
+	cout << "Masukkan angka : ";
+	cin >> pilihan;
+	
+	if(pilihan == 1){
+		menuMap();
+	}
 
     return 0;
 }
---
+++ map.h
@@ -0,0 +1,7 @@
+#ifndef map_H
+#define map_H
+
+void menuMap();
+void mapSatu();
+
+#endif
