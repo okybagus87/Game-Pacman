133566b85549c819712be36d0fad608ee0558f3e
yosualr <80729336+yosualr@users.noreply.github.com>
2021-07-05 05:14:19 +0700

    Update enemy.h

--- Pacman/header/enemy.h
+++ Pacman/header/enemy.h
@@ -2,11 +2,24 @@
 #define ENEMY_H_INCLUDED
 #include <iostream>
 #include <stdlib.h>
+#include<graphics.h>
+#include<dos.h>
+#include<stdio.h>
+#include<conio.h>
+#define Gimg_mtrx 40*j_g,40*i_g,40+40*j_g,40+40*i_g
 
-void DrawGhost(int i, int j, int p);
-int kondisi(int px, int py, int gx, int gy);
-int moveGhostX(int situasi,int *gx, int *gy);
-int moveGhostY(int situasi,int *gx, int *gy);
+
+int checkPosisiPac(int i,int j,int i_g, int j_g);
+int xGhostDir(int i,int i_g,int j_g, int j);
+int YGhostDir(int i,int i_g, int j, int j_g);
+
+
+void ceksprite(int sprite, int baris, int kolom);
+void Gmaping();
+void Glimit();
+void Gmovement(int l);
+void Gmove(bool g_atas, bool g_bawah, bool g_kiri, bool g_kanan);
+void Gceksprite(int sprite, int baris, int kolom);
 
 
 #endif // ENEMY_H_INCLUDED
