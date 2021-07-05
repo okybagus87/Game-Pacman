bf1d6c9629769ed75129320e835f4b4ff6941122
ginaanifahchoirunnisa <80727367+ginaanifahchoirunnisa@users.noreply.github.com>
2021-07-05 08:25:49 +0700

    kontrol seluruh header
    seluruh header didefinisikan disini

---
+++ header.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+#include<stdio.h>
+#include<graphics.h>
+#include<conio.h>
+#include<time.h>
+#include<windows.h>
+#include<process.h>
+#include<stdbool.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<mmsystem.h>
+#define fullscreen 0,0,getmaxx(),getmaxy()
+
+
+using namespace std;
+
+
+
+#endif // HEADER_H_INCLUDED
