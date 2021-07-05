53b4ff0512c5d9b12d0ae3f60d316a5d175fceb4
ginaanifahchoirunnisa <80727367+ginaanifahchoirunnisa@users.noreply.github.com>
2021-07-05 11:25:27 +0700

    file ppl linked list

---
+++ palleteGame/main.cpp
@@ -0,0 +1,17 @@
+
+#include "header.h"
+
+using namespace std;
+
+
+
+int main()
+{
+    initwindow(1280,720,"pacman_game");
+
+    menu_utama();
+
+    getchar();
+    closegraph();
+    return 0;
+}
---
+++ palleteGame/sound.h
@@ -0,0 +1,11 @@
+#ifndef SOUND_H_INCLUDED
+#define SOUND_H_INCLUDED
+#include "header.h"
+#include<mmsystem.h>
+
+
+
+
+
+
+#endif // SOUND_H_INCLUDED
