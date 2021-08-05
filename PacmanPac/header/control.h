8195a3fa4a89518a6e6e829f504bd20287b357a0
salsabilaputrf <80728365+salsabilaputrf@users.noreply.github.com>
2021-08-05 07:53:31 +0700

    Update control.h
    menambahkan modul untuk rekap score

--- PacmanPac/header/control.h
+++ PacmanPac/header/control.h
@@ -1,5 +1,11 @@
 //#ifndef control_H
 //#define control_H
+
+typedef struct{
+	char name[20];
+	int hscore;
+}Score;
+
 void SkorPemain(int skor);//MENAMPILKAN SKOR
 void mapMaze(int l); // MEMANGGIL MAPS DAN MENAMPILKANNYA
 void tempMaps(int temp, int baris, int kolom); // MEMBUAT TEMPORARY MAPS
@@ -11,4 +17,8 @@ void movement(int l); // MENGGERAKAN PACMAN
 void maping(); // POSISI AWAL PACMAN
 void pause(); // FITUR PAUSE
 void sc_lv(int l); // MENAMPILAKAN JUDUL SAAT KE MAPS TERTENTU
+void rekap_score(); //MEREKAP SCORE
+void entry_score(); //MEMASUKAN SKOR KE FILE SCORE.DAT
+void Tampil_score(); //MENAMPILKAN DATA YANG TERDAPAT PADA FILE SCORE.DAT
+void sort_score(); //MENGURUTKAN SKOR
 //#endif
